9f80bfbe5ce3f33f6c33adc708bbbeafc6f55960
cevher kemal sirin <76917072+cevherkemalsirin@users.noreply.github.com>
2024-01-16 23:40:18 +0300

    Create JS operate functions

---
+++ script.js
@@ -0,0 +1,40 @@
+"use strict"
+
+// left hand side value and right hand value with the operator in between i.e. 2 + 4 
+let LeftValue, operator, rightValue;
+
+// 4 operators
+function Add(a,b)
+{
+    return a + b;
+}
+
+function Substract(a,b)
+{
+    return a - b;
+}
+
+function Divide(a,b)
+{
+    return a / b;
+}
+
+function Multiply(a,b)
+{
+    return a * b;
+}
+
+function Modulus(a,b)
+{
+    return a % b;
+}
+
+function Power(a,b)
+{
+    return a ** b;
+}
+
+function Operate (rValue, operator, lValue)
+{
+
+}
